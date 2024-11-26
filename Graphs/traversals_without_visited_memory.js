44861cd3ea1cdc51ec549a978a72ea27e35156bd
Jamal Arshad <72703812+JamalArshad147@users.noreply.github.com>
2024-11-26 13:40:30 +0000

    files names updated!

--- Graphs/traversals_without_visited_memory.js
+++ Graphs/traversals_without_visited_memory.js
@@ -1,6 +1,6 @@
-import { sGraph } from "./graphs.js";
-import { mGraph } from "./graphs.js";
-import { lGraph } from "./graphs.js";
+import { sGraph } from "./graphs_object.js";
+import { mGraph } from "./graphs_object.js";
+import { lGraph } from "./graphs_object.js";
 
 
 // ********************** these traversals don't keep track of visited nodes *************************
@@ -9,6 +9,18 @@ import { lGraph } from "./graphs.js";
 // DFS_Iterative(sGraph, "A");
 // DFS_Recursive(sGraph, "A");
 
+function BFS(graph, src) {
+  let queue = [src];
+  console.log(`***** Breadth First Search *****`);
+  while (queue.length > 0) {
+    let current = queue.shift();
+    console.log(current);
+    for (const neighbor of graph[current]) {
+      queue.push(neighbor);
+    }
+  }
+  console.log(`----------------------------------`);
+}
 
 function DFS_Recursive(graph, src) {
   console.log(src);
